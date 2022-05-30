9117a89d38a35bccc44632b7051b1bc967b2605f
rbozhko <roman.bozhko@globallogic.com>
2022-05-30 20:47:05 +0300

    resolve #1; create all necessary pages

---
+++ lib/pages/view_card_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewCardPage extends StatelessWidget {
+  const ViewCardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
